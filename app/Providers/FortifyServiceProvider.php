f445a89f87bf389f338cbc7c21d346374f47a3c5
Justin Navarro <info@justinnavarro.net>
2020-12-24 15:37:41 -0500

    add and setup fortify

---
+++ app/Providers/FortifyServiceProvider.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Providers;
+
+use App\Actions\Fortify\CreateNewUser;
+use App\Actions\Fortify\ResetUserPassword;
+use App\Actions\Fortify\UpdateUserPassword;
+use App\Actions\Fortify\UpdateUserProfileInformation;
+use Illuminate\Support\ServiceProvider;
+use Inertia\Inertia;
+use Laravel\Fortify\Fortify;
+
+class FortifyServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Fortify::createUsersUsing(CreateNewUser::class);
+        Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
+        Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
+        Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
+
+        Fortify::loginView(function () {
+            return Inertia::render('auth.Login');
+        });
+
+        Fortify::registerView(function () {
+            return Inertia::render('auth.Register');
+        });
+
+        Fortify::requestPasswordResetLinkView(function () {
+            return Inertia::render('auth.RequestPasswordReset');
+        });
+
+        Fortify::resetPasswordView(function () {
+            return Inertia::render('auth.ResetPassword');
+        });
+    }
+}
---
+++ resources/js/Pages/auth/RequestPasswordReset.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const RequestPasswordReset = () => {
+    return (
+        <h1>RequestPasswordReset</h1>
+    );
+};
+
+export default RequestPasswordReset;
