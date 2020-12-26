34c76f0d5a6d0c44994da17e64ca0f0285cfc232
Justin Navarro <info@justinnavarro.net>
2020-12-26 15:23:35 -0500

    started building login page

--- resources/js/Pages/auth/Login.tsx
+++ resources/js/Pages/auth/Login.tsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 // @ts-ignore
 import Guest from '@/Pages/layouts/Guest';
+import CustomInput from '../../components/ui-elements/form/CustomInput';
+import FormContextProvider from '../../components/ui-elements/form/FormContextProvider';
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [isValid, setIsValid] = useState(false);
+
     return (
         <Guest>
-            <h1>Login</h1>
+            <h1
+                className="text-center text-2xl text-gray-800 sm:text-gray-600 font-header mb-8"
+            >
+                Welcome Back
+            </h1>
+
+            <FormContextProvider handleUpdateValid={setIsValid} valid={isValid}>
+                <CustomInput
+                    handleUpdateInput={setEmail}
+                    label="Email"
+                    rules={['required', 'email']}
+                    value={email}
+                />
+
+                <CustomInput
+                    handleUpdateInput={setPassword}
+                    label="Password"
+                    rules={['required']}
+                    type="password"
+                    value={password}
+                />
+            </FormContextProvider>
         </Guest>
     );
 };
--- resources/js/Pages/layouts/Guest.tsx
+++ resources/js/Pages/layouts/Guest.tsx
@@ -22,7 +22,7 @@ const Guest: React.FC<GuestProps> = ({ children }) => {
 			<div className="absolute top-0 left-0 w-full h-full bg-gray-100 sm:bg-primary sm:bg-opacity-75 z-20" />
 			<div style={styles.bgImage} className="absolute left-0 top-0 w-full h-full z-10" />
 
-			<div className="relative z-30 w-full sm:w-100 mt-6 px-4 sm:px-0 pt-4 sm:bg-white sm:rounded-lg sm:shadow-md overflow-hidden text-left">
+			<div className="relative z-30 w-full sm:w-100 mt-6 px-4 pt-4 sm:bg-white sm:rounded-lg sm:shadow-md overflow-hidden text-left">
 				<div className="flex flex-row justify-center mb-4">
 					<div className="overflow-hidden h-16 w-16">
 						<img
