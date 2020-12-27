70a0492110d39056a5d90d4ee5c32681952e59c7
Justin Navarro <info@justinnavarro.net>
2020-12-27 01:03:09 -0500

    registration page build

--- resources/js/Pages/auth/Login.tsx
+++ resources/js/Pages/auth/Login.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import { InertiaLink } from '@inertiajs/inertia-react'
 
 // @ts-ignore
 import Guest from '@/Pages/layouts/Guest';
 import CustomButton from '../../components/ui-elements/form/CustomButton';
+import CustomCheckbox from '../../components/ui-elements/form/CustomCheckbox';
 import CustomInput from '../../components/ui-elements/form/CustomInput';
 import FormContextProvider from '../../components/ui-elements/form/FormContextProvider';
 
@@ -17,30 +19,56 @@ const Login = () => {
 				Welcome Back
 			</h1>
 
-			<FormContextProvider handleUpdateValid={setIsValid} valid={isValid}>
-				<CustomInput
-					handleUpdateInput={setEmail}
-					label="Email"
-					rules={['required', 'email']}
-					value={email}
-				/>
+            <div className="px-4">
+                <FormContextProvider handleUpdateValid={setIsValid} valid={isValid}>
+                    <CustomInput
+                        handleUpdateInput={setEmail}
+                        label="Email"
+                        rules={['required', 'email']}
+                        value={email}
+                    />
 
-				<CustomInput
-					handleUpdateInput={setPassword}
-					label="Password"
-					rules={['required']}
-					type="password"
-					value={password}
-				/>
+                    <CustomInput
+                        handleUpdateInput={setPassword}
+                        label="Password"
+                        rules={['required']}
+                        type="password"
+                        value={password}
+                    />
 
-				<CustomButton
-					block
-					color="secondary"
-					handleClick={() => console.log('peace')}
-				>
-					Login
-				</CustomButton>
-			</FormContextProvider>
+                    <div className="my-4">
+                        <CustomCheckbox
+                            handleUpdateInput={() => console.log('checked')}
+                            label="Remember me"
+                            value="akdk"
+                        />
+                    </div>
+
+                    <CustomButton
+                        block
+                        color="secondary"
+                        handleClick={() => console.log('peace')}
+                    >
+                        Login
+                    </CustomButton>
+                </FormContextProvider>
+            </div>
+
+            <div
+                className="mt-6 py-4 px-4 sm:bg-gray-100 flex flex-row justify-center sm:justify-between items-center"
+            >
+                <InertiaLink
+                    className="text-gray-700 underline text-sm hover:no-underline"
+                    href="/register">
+                    Not Registered?
+                </InertiaLink>
+
+                <InertiaLink
+                    className="text-gray-700 underline text-sm hover:no-underline"
+                    href="/forgot-password">
+                    Forgot your password?
+                </InertiaLink>
+            </div>
 		</Guest>
 	);
 };
--- resources/js/Pages/auth/Register.tsx
+++ resources/js/Pages/auth/Register.tsx
@@ -1,8 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { InertiaLink } from '@inertiajs/inertia-react';
+
+// @ts-ignore
+import Guest from '@/Pages/layouts/Guest';
+import CustomButton from '../../components/ui-elements/form/CustomButton';
+import CustomInput from '../../components/ui-elements/form/CustomInput';
+import FormContextProvider from '../../components/ui-elements/form/FormContextProvider';
 
 const Register = () => {
-    return (
-        <h1>Register</h1>
+	const [email, setEmail] = useState('');
+	const [name, setName] = useState('');
+	const [confirmPassword, setConfirmPassword] = useState('');
+	const [password, setPassword] = useState('');
+	const [isValid, setIsValid] = useState(false);
+
+	return (
+	    <Guest>
+            <h1 className="text-center text-2xl text-gray-800 sm:text-gray-600 font-header mb-8">
+                Welcome Back
+            </h1>
+
+            <div className="px-4">
+                <FormContextProvider handleUpdateValid={setIsValid} valid={isValid}>
+                    <CustomInput
+                        handleUpdateInput={setName}
+                        label="Name"
+                        rules={['required']}
+                        value={name}
+                    />
+
+                    <CustomInput
+                        handleUpdateInput={setEmail}
+                        label="Email"
+                        rules={['required', 'email']}
+                        value={email}
+                    />
+
+                    <CustomInput
+                        handleUpdateInput={setPassword}
+                        label="Password"
+                        rules={['required']}
+                        type="password"
+                        value={password}
+                    />
+
+                    <CustomInput
+                        handleUpdateInput={setConfirmPassword}
+                        label="Confirm Password"
+                        rules={['required']}
+                        type="password"
+                        value={confirmPassword}
+                    />
+
+                    <CustomButton
+                        block
+                        color="secondary"
+                        handleClick={() => console.log('peace')}
+                    >
+                        Register
+                    </CustomButton>
+                </FormContextProvider>
+            </div>
+
+            <div
+                className="mt-6 py-4 px-4 sm:bg-gray-100 flex flex-row justify-center sm:justify-end items-center"
+            >
+                <InertiaLink
+                    className="text-gray-700 underline text-sm hover:no-underline"
+                    href="/login">
+                    Already registered?
+                </InertiaLink>
+            </div>
+        </Guest>
     );
 };
 
