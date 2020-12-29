e5f2712fc951f1b457faf3badb755ea75504c3cf
Justin Navarro <info@justinnavarro.net>
2020-12-28 22:36:10 -0500

    create post request to register a user

--- resources/js/Pages/auth/Register.tsx
+++ resources/js/Pages/auth/Register.tsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
-import { InertiaLink } from '@inertiajs/inertia-react';
+import React, { BaseSyntheticEvent, useEffect, useState } from 'react';
+import { InertiaLink, usePage } from '@inertiajs/inertia-react';
 
-// @ts-ignore
-import Guest from '@/Pages/layouts/Guest';
+import Alert from '../../components/ui-elements/Alert';
+import Guest from '../layouts/Guest';
 import CustomButton from '../../components/ui-elements/form/CustomButton';
 import CustomInput from '../../components/ui-elements/form/CustomInput';
 import FormContextProvider from '../../components/ui-elements/form/FormContextProvider';
+import { CustomProps } from '../../@types/custom-inertia';
+import { Inertia } from '@inertiajs/inertia';
 
 const Register = () => {
 	const [email, setEmail] = useState('');
@@ -13,66 +15,84 @@ const Register = () => {
 	const [confirmPassword, setConfirmPassword] = useState('');
 	const [password, setPassword] = useState('');
 	const [isValid, setIsValid] = useState(false);
+	const [loginErrors, setLoginErrors] = useState<string[]>([]);
+	const { errors, csrf_token } = usePage().props as CustomProps;
+
+	useEffect(() => {
+		setLoginErrors(Object.values(errors));
+	}, [errors]);
+
+	const handleSubmit = async (e: BaseSyntheticEvent) => {
+		e.preventDefault();
+		await Inertia.post('/register', {
+			email,
+			name,
+			password,
+			password_confirmation: confirmPassword,
+			_token: csrf_token,
+		});
+	};
 
 	return (
-	    <Guest>
-            <h1 className="text-center text-2xl text-gray-800 sm:text-gray-600 font-header mb-8">
-                Welcome Back
-            </h1>
+		<Guest>
+			<h1 className="text-center text-2xl text-gray-800 sm:text-gray-600 font-header mb-8">
+				Welcome Back
+			</h1>
 
-            <div className="px-4">
-                <FormContextProvider handleUpdateValid={setIsValid} valid={isValid}>
-                    <CustomInput
-                        handleUpdateInput={setName}
-                        label="Name"
-                        rules={['required']}
-                        value={name}
-                    />
+			<div className="px-4">
+				<Alert errors={loginErrors} type="error" />
 
-                    <CustomInput
-                        handleUpdateInput={setEmail}
-                        label="Email"
-                        rules={['required', 'email']}
-                        value={email}
-                    />
+				<FormContextProvider
+					handleSubmit={handleSubmit}
+					handleUpdateValid={setIsValid}
+					valid={isValid}
+				>
+					<CustomInput
+						handleUpdateInput={setName}
+						label="Name"
+						rules={['required']}
+						value={name}
+					/>
 
-                    <CustomInput
-                        handleUpdateInput={setPassword}
-                        label="Password"
-                        rules={['required']}
-                        type="password"
-                        value={password}
-                    />
+					<CustomInput
+						handleUpdateInput={setEmail}
+						label="Email"
+						rules={['required', 'email']}
+						value={email}
+					/>
 
-                    <CustomInput
-                        handleUpdateInput={setConfirmPassword}
-                        label="Confirm Password"
-                        rules={['required']}
-                        type="password"
-                        value={confirmPassword}
-                    />
+					<CustomInput
+						handleUpdateInput={setPassword}
+						label="Password"
+						rules={['required']}
+						type="password"
+						value={password}
+					/>
 
-                    <CustomButton
-                        block
-                        color="secondary"
-                        handleClick={() => console.log('peace')}
-                    >
-                        Register
-                    </CustomButton>
-                </FormContextProvider>
-            </div>
+					<CustomInput
+						handleUpdateInput={setConfirmPassword}
+						label="Confirm Password"
+						rules={['required']}
+						type="password"
+						value={confirmPassword}
+					/>
 
-            <div
-                className="mt-6 py-4 px-4 sm:bg-gray-100 flex flex-row justify-center sm:justify-end items-center"
-            >
-                <InertiaLink
-                    className="text-gray-700 underline text-sm hover:no-underline"
-                    href="/login">
-                    Already registered?
-                </InertiaLink>
-            </div>
-        </Guest>
-    );
+					<CustomButton block color="secondary" submit>
+						Register
+					</CustomButton>
+				</FormContextProvider>
+			</div>
+
+			<div className="mt-6 py-4 px-4 sm:bg-gray-100 flex flex-row justify-center sm:justify-end items-center">
+				<InertiaLink
+					className="text-gray-700 underline text-sm hover:no-underline"
+					href="/login"
+				>
+					Already registered?
+				</InertiaLink>
+			</div>
+		</Guest>
+	);
 };
 
 export default Register;
