e5ad94860e17cf877c22231d01c48dcfa116f636
Justin Navarro <info@justinnavarro.net>
2020-12-31 00:32:16 -0500

    update input styling

--- resources/js/components/ui-elements/form/CustomInput.tsx
+++ resources/js/components/ui-elements/form/CustomInput.tsx
@@ -50,7 +50,7 @@ const CustomInput: React.FC<CustomInputProps> = ({
 				id={labelId}
 				className={`w-full p-2 mt-2 border rounded outline-none ${
 					!error
-						? 'border-gray-200 focus:border-primary'
+						? 'border-gray-300 focus:border-primary'
 						: 'border-red-600'
 				}`}
 				type={type}
