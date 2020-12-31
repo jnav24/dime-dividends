7e8cda9d3904226c9f5091df4b88d6868e95ee3e
Justin Navarro <info@justinnavarro.net>
2020-12-31 00:32:03 -0500

    created modals

---
+++ resources/js/components/modals/HoldingsModal.tsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react';
+
+import CustomButton from '../ui-elements/form/CustomButton';
+import CustomInput from '../ui-elements/form/CustomInput';
+import FormContextProvider from '../ui-elements/form/FormContextProvider';
+import Modal from './Modal';
+
+type HoldingsModalType = {};
+
+const HoldingsModal: React.FC<HoldingsModalType> = () => {
+	const [isValid, setIsValid] = useState(false);
+	const [shares, setShares] = useState('');
+	const [sharePrice, setSharePrice] = useState('');
+	const [ticker, setTicker] = useState('');
+	const closeModal = () => {};
+
+    useEffect(() => {
+        return () => {
+            setTicker('');
+            setShares('');
+            setSharePrice('');
+            setIsValid(false);
+        };
+    });
+
+	return (
+		<Modal>
+			<div className="w-100">
+				<div className="bg-gray-100 pl-2 py-2 text-2xl text-gray-700 font-header">
+					Add Holding <pre>{isValid}</pre>
+				</div>
+
+				<FormContextProvider valid={isValid} handleUpdateValid={setIsValid}>
+					<div className="py-4 px-2">
+						<CustomInput
+							handleUpdateInput={setTicker}
+							label="Ticker"
+                            rules={['required']}
+							value={ticker}
+						/>
+
+						<CustomInput
+							handleUpdateInput={setShares}
+							label="Shares"
+                            rules={['required', 'numeric']}
+							value={shares}
+						/>
+
+						<CustomInput
+							handleUpdateInput={setSharePrice}
+							label="Cost Per Share"
+                            rules={['required', 'float:2']}
+							value={sharePrice}
+						/>
+					</div>
+
+					<div className="bg-gray-100 flex-row flex justify-end py-2">
+						<CustomButton>Cancel</CustomButton>
+						<CustomButton color="secondary" isDisabled={!isValid}>
+							Add Holding
+						</CustomButton>
+					</div>
+				</FormContextProvider>
+			</div>
+		</Modal>
+	);
+};
+
+export default HoldingsModal;
---
+++ resources/js/components/modals/Modal.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+type ModalType = {}
+
+const Modal: React.FC<ModalType> = ({ children }) => {
+    const closeModal = () => {};
+
+    return (
+        <div className="fixed z-100 inset-0 overflow-y-auto">
+            <div
+                className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
+            >
+                <div className="fixed inset-0">
+                    <div
+                        className="absolute inset-0 bg-black opacity-75"
+                        onClick={closeModal}
+                    />
+                </div>
+
+                {/* This element is to trick the browser into centering the modal contents. */}
+                <span
+                    className="hidden sm:inline-block sm:align-middle sm:h-screen"
+                />
+                &#8203;
+
+                <div
+                    className="inline-block align-bottom bg-white overflow-x-hidden rounded-lg text-left shadow-xl transform transition-all sm:my-8 sm:align-middle"
+                    role="dialog"
+                    aria-modal="true"
+                    aria-labelledby="modal-content"
+                >
+                    {children}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Modal;
