c06e309c8eed8faeee51387a5167a45e239f2b72
Justin Navarro <info@justinnavarro.net>
2021-01-04 17:50:18 -0500

    get holdings from db on index

--- app/Http/Controllers/HoldingsController.php
+++ app/Http/Controllers/HoldingsController.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserDividend;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 use Illuminate\Support\Facades\Http;
 
 class HoldingsController extends Controller
 {
-    public function searchByTicker($ticker)
+    public function index(): \Inertia\Response
+    {
+        $holdings = UserDividend::with(['dividend'])->where('user_id', auth()->user()->id)->get();
+
+        return Inertia::render('Dashboard', [
+            'holdings' => $holdings,
+        ]);
+    }
+
+    public function searchByTicker($ticker): array
     {
         $response = Http::get('https://seekingalpha.com/api/common/ac/search?limit=5&symbols=1&term=' . $ticker);
         return $response->json()['symbols'] ?? [];
