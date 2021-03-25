25c6ebda0a39cb871a442436b4b81ab532778292
Justin Navarro <info@justinnavarro.net>
2021-03-25 17:26:14 -0400

    created Update Payout Service

--- app/Services/UpdatePayoutService.php
+++ app/Services/UpdatePayoutService.php
@@ -2,8 +2,21 @@
 
 namespace App\Services;
 
+use App\Models\Dividend;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
+
 class UpdatePayoutService
 {
-    public function __invoke()
-    {}
+    public function __invoke(SeekingAlphaService $seekingAlphaService)
+    {
+        $dividends = Dividend::whereDate('next_payout_at', '<=', Carbon::now())->get();
+        $dividends->map(function ($dividend) use ($seekingAlphaService) {
+            $data = $seekingAlphaService::getHoldingDetails($dividend->ticker);
+            $dividend->next_payout_at = Carbon::createFromFormat('Y-m-d', $data['next-payout']);
+            $dividend->save();
+        });
+
+        Log::info('Payouts updated: ' . json_encode($dividends->pluck('ticker')));
+    }
 }
