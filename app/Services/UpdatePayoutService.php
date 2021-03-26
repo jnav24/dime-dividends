c52cdcde195b5b317d3a38fbd46fb91609382ba9
Justin Navarro <info@justinnavarro.net>
2021-03-25 20:00:05 -0400

    prevent errors if data returns null

--- app/Services/UpdatePayoutService.php
+++ app/Services/UpdatePayoutService.php
@@ -11,10 +11,12 @@ class UpdatePayoutService
     public function __invoke(SeekingAlphaService $seekingAlphaService)
     {
         $dividends = Dividend::whereDate('next_payout_at', '<=', Carbon::now())->get();
-        $dividends->map(function ($dividend) use ($seekingAlphaService) {
-            $data = $seekingAlphaService::getHoldingDetails($dividend->ticker);
-            $dividend->next_payout_at = Carbon::createFromFormat('Y-m-d', $data['next-payout']);
-            $dividend->save();
+        $dividends->each(function ($dividend) use ($seekingAlphaService) {
+            $data = $seekingAlphaService->getHoldingDetails($dividend->ticker);
+            if (!empty($data)) {
+                $dividend->next_payout_at = Carbon::createFromFormat('Y-m-d', $data['next-payout']);
+                $dividend->save();
+            }
         });
 
         Log::info('Payouts updated: ' . json_encode($dividends->pluck('ticker')));
