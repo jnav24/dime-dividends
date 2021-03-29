adb5c42183aef52f553cdf09d581dcb4021ce9a2
Justin Navarro <info@justinnavarro.net>
2021-03-29 12:39:56 -0400

    add settings page

---
+++ resources/js/Pages/Settings.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Auth from './layouts/Auth';
+
+type Props = {};
+
+const Settings: React.FC<Props> = () => {
+    return (
+        <Auth>
+            <h1>Soon to be Settings page</h1>
+        </Auth>
+    );
+};
+
+export default Settings;
