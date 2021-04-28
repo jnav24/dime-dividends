d61a51d6e95ddb6de533d79603a8e175f0b5bb87
Justin Navarro <info@justinnavarro.net>
2021-04-28 18:57:57 -0400

    password confirm settings page

--- routes/web.php
+++ routes/web.php
@@ -26,19 +26,18 @@ Route::middleware(['auth', 'verified'])->group(function () {
         Route::get('/income', function () {
             return Inertia::render('Income');
         })->name('income');
+        Route::get('settings', [SettingsController::class, 'index']);
+        Route::post('settings/profile', [SettingsController::class, 'updateProfileInformation']);
+        Route::post('settings/password', [SettingsController::class, 'updatePassword']);
+        Route::delete('user/two-factor-authentication', [TwoFactorAuthenticationController::class, 'destroy']);
+        Route::post('user/two-factor-authentication', [TwoFactorAuthenticationController::class, 'store']);
+        Route::get('user/two-factor-qr-code', [TwoFactorAuthenticationController::class, 'show']);
     });
 
     Route::get('dashboard', [HoldingsController::class, 'index'])->name('dashboard');
     Route::post('add-holding', [HoldingsController::class, 'store'])->name('add-holding');
     Route::post('update-holding/{id}', [HoldingsController::class, 'update'])->name('update-holding');
-
     Route::get('search/{ticker}', [HoldingsController::class, 'searchByTicker']);
-    Route::get('settings', [SettingsController::class, 'index']);
-    Route::post('settings/profile', [SettingsController::class, 'updateProfileInformation']);
-    Route::post('settings/password', [SettingsController::class, 'updatePassword']);
-    Route::delete('user/two-factor-authentication', [TwoFactorAuthenticationController::class, 'destroy']);
-    Route::post('user/two-factor-authentication', [TwoFactorAuthenticationController::class, 'store']);
-    Route::get('user/two-factor-qr-code', [TwoFactorAuthenticationController::class, 'show']);
 });
 
 require __DIR__.'/auth.php';
