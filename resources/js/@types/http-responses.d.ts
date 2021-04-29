903754b8b6bce0667004044f002e9d4720e5b06c
Justin Navarro <info@justinnavarro.net>
2021-04-29 15:46:21 -0400

    fixed node bug with exporting enum

--- resources/js/@types/http-responses.d.ts
+++
@@ -1,8 +0,0 @@
-declare namespace HttpResponses {
-    enum HttpError {
-        DEFAULT = 'Something unexpected had occurred.',
-        PASSWORD_CONFIRM = 'Password confirmation required.'
-    }
-}
-
-export = HttpResponses;
---
+++ resources/js/@types/http-responses.ts
@@ -0,0 +1,4 @@
+export enum HttpError {
+    DEFAULT = 'Something unexpected had occurred.',
+    PASSWORD_CONFIRM = 'Password confirmation required.'
+}
