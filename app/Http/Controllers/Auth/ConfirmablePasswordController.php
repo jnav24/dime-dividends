0b7b92bdca4a2e114270caac658661cd95087d53
Justin Navarro <info@justinnavarro.net>
2021-04-29 11:21:48 -0400

    add password confirmation error message

--- app/Http/Controllers/Auth/ConfirmablePasswordController.php
+++ app/Http/Controllers/Auth/ConfirmablePasswordController.php
@@ -36,7 +36,7 @@ class ConfirmablePasswordController extends Controller
             'password' => $request->password,
         ])) {
             throw ValidationException::withMessages([
-                'password' => __('auth.password'),
+                'password' => 'The given credentials are incorrect.',
             ]);
         }
 
