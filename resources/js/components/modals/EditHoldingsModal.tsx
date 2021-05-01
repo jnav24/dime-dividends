7738646796810dc45367f8a41d1a28aa95d67615
Justin Navarro <info@justinnavarro.net>
2021-05-01 16:18:44 -0400

    build out the edit modal further

--- resources/js/components/modals/EditHoldingsModal.tsx
+++ resources/js/components/modals/EditHoldingsModal.tsx
@@ -1,68 +1,91 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 import Modal from './Modal';
 import CustomButton from '../ui-elements/form/CustomButton';
+import CustomInput from '../ui-elements/form/CustomInput';
 import CustomRadio from '../ui-elements/form/CustomRadio';
-import FormContextProvider from '../ui-elements/form/FormContextProvider';
 import CustomRadioGroup from '../ui-elements/form/CustomRadioGroup';
+import FormContextProvider from '../ui-elements/form/FormContextProvider';
+import { HoldingsModalType } from '../../@types/holdings';
+import useTimestamp  from '../../hooks/useTimestamp';
 
-type Props = {
-    handleShowModal: (e: boolean) => void;
-    show: boolean;
-};
+const EditHoldingsModal: React.FC<HoldingsModalType> = ({ data, handleAddHolding, handleShowModal, show }) => {
+    const { formatDate } = useTimestamp();
+	const [animateCloseModal, setAnimateCloseModal] = useState(false);
+	const [cost, setCost] = useState('');
+	const [quantity, setQuantity] = useState('');
+	const [isValid, setIsValid] = useState(false);
 
-const EditHoldingsModal: React.FC<Props> = ({ handleShowModal, show }) => {
-    const [animateCloseModal, setAnimateCloseModal] = useState(false);
-    const [isValid, setIsValid] = useState(false);
+	return (
+		<Modal
+			handleShowModal={handleShowModal}
+			show={show}
+			animateCloseModal={animateCloseModal}
+		>
+			<div className="w-200">
+				<div className="bg-gray-100 pl-2 py-2 text-2xl text-gray-700 font-header">
+					Update Holding
+				</div>
 
-    return (
-        <Modal handleShowModal={handleShowModal} show={show} animateCloseModal={animateCloseModal}>
-            <div className="w-200">
-                <div className="bg-gray-100 pl-2 py-2 text-2xl text-gray-700 font-header">
-                    Update Holding
-                </div>
+				<FormContextProvider
+					valid={isValid}
+					handleUpdateValid={setIsValid}
+				>
+					<div className="py-6 px-4 flex flex-row justify-between items-start">
+						<div className="text-gray-600">
+                            <div className="mb-8">
+                                <h2 className="text-xl text-black">{data.name}</h2>
+                                <p className="text-sm">({data.ticker})</p>
+                            </div>
+                            <p>Next Payout Date: {formatDate('MM/dd/yyyy', data.next_payout_at)}</p>
+                            <p>Current Holdings: {data.quantity}</p>
+                            <p>Current Price: </p>
+						</div>
 
-                <FormContextProvider
-                    valid={isValid}
-                    handleUpdateValid={setIsValid}
-                >
-                    <div className="py-4 px-2 flex flex-row justify-between items-start">
-                        <div>
-                            Name/Ticker
-                            Current Price
-                            Quantity
-                        </div>
+						<div className="space-y-2 w-2/4">
+							<CustomRadioGroup
+								label="Order Type"
+								row
+								defaultValue="bought"
+							>
+								<CustomRadio label="Bought" value="bought" />
+								<CustomRadio label="Sold" value="sold" />
+							</CustomRadioGroup>
+							<CustomInput
+								handleUpdateInput={setCost}
+                                rules={['required', 'float:2']}
+								value={cost}
+								label="Cost Per Share"
+							/>
+							<CustomInput
+								handleUpdateInput={setQuantity}
+                                rules={['required', 'float:2']}
+								value={quantity}
+								label="Quantity"
+							/>
+						</div>
+					</div>
 
-                        <div>
-                            <CustomRadioGroup label="Order Type" row defaultValue="bought">
-                                <CustomRadio label="Bought" value="bought" />
-                                <CustomRadio label="Sold" value="sold" />
-                            </CustomRadioGroup>
-                            Cost
-                            Amount
-                        </div>
-                    </div>
+					<div className="bg-gray-100 flex-row flex justify-end py-2">
+						<CustomButton
+							handleClick={() => setAnimateCloseModal(true)}
+							ignoreValidation
+						>
+							Cancel
+						</CustomButton>
 
-                    <div className="bg-gray-100 flex-row flex justify-end py-2">
-                        <CustomButton
-                            handleClick={() => setAnimateCloseModal(true)}
-                            ignoreValidation
-                        >
-                            Cancel
-                        </CustomButton>
-
-                        <CustomButton
-                            color="secondary"
-                            handleClick={() => setAnimateCloseModal(true)}
-                            isDisabled={!isValid}
-                        >
-                            Update
-                        </CustomButton>
-                    </div>
-                </FormContextProvider>
-            </div>
-        </Modal>
-    );
+						<CustomButton
+							color="secondary"
+							handleClick={() => setAnimateCloseModal(true)}
+							isDisabled={!isValid}
+						>
+							Update
+						</CustomButton>
+					</div>
+				</FormContextProvider>
+			</div>
+		</Modal>
+	);
 };
 
 export default EditHoldingsModal;
